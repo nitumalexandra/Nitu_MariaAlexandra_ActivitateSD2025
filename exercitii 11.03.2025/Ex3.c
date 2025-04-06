1318e41378e0561ef1d4e8658100da6261fcb28e
Nitu Maria Alexandra <alexxandranitzu@gmail.com>
2025-04-06 22:13:56 +0300

    ex 3 - 06.04.2025

---
+++ exercitii 11.03.2025/Ex3.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+bool estePrim(int nr) {
+    if (nr <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= nr; i++) {
+        if (nr % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void prim(int nr) {
+    printf("Factorii primi ai numarului %d sunt: ", nr);
+
+    for (int i = 2; i <= nr; i++) {
+        if (nr % i == 0 && estePrim(i)) {
+            printf("%d ", i);
+            while (nr % i == 0) {
+                nr /= i;
+            }
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int nr;
+    printf("Introduceti un numar: ");
+    scanf("%d", &nr);
+    prim(nr);
+
+    return 0;
+}
